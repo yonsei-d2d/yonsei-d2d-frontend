21a674e5b138faab6748e9a0eb01f3320c3d7c76
Myeonghoon Lee <11175606+mhl6829@users.noreply.github.com>
2024-12-03 02:10:51 +0900

    [add] New location

--- src/utils/category-tag-util.tsx
+++ src/utils/category-tag-util.tsx
@@ -2,31 +2,64 @@ import { Badge } from "react-bootstrap";
 import { LocationType } from "../enums/location-type.enum";
 import styled from "styled-components";
 
-
 const Tag = styled.div`
-  background-color: #79A8DD;
+  background-color: #79a8dd;
   color: white;
   padding: 0 10px;
   border-radius: 10px;
-`
+`;
 
 export const CategoryTagUtil = ({ type }: { type: LocationType }) => {
   const getTag = (type: LocationType) => {
     switch (type) {
       case LocationType.BUILDING:
-        return <Tag><small>건물</small></Tag>;
+        return (
+          <Tag>
+            <small>건물</small>
+          </Tag>
+        );
       case LocationType.CAFE:
-        return <Tag><small>카페</small></Tag>;
+        return (
+          <Tag>
+            <small>카페</small>
+          </Tag>
+        );
       case LocationType.CAFETERIA:
-        return <Tag><small>학식당</small></Tag>;
+        return (
+          <Tag>
+            <small>학식당</small>
+          </Tag>
+        );
       case LocationType.CONVENIENCE:
-        return <Tag><small>편의점</small></Tag>;
+        return (
+          <Tag>
+            <small>편의점</small>
+          </Tag>
+        );
       case LocationType.RESTAURANT:
-        return <Tag><small>식당</small></Tag>;
+        return (
+          <Tag>
+            <small>식당</small>
+          </Tag>
+        );
       case LocationType.STORE:
-        return <Tag><small>상점</small></Tag>;
+        return (
+          <Tag>
+            <small>상점</small>
+          </Tag>
+        );
+      case LocationType.FACILITY:
+        return (
+          <Tag>
+            <small>시설</small>
+          </Tag>
+        );
       default:
-        return <Tag><small>장소</small></Tag>;;
+        return (
+          <Tag>
+            <small>장소</small>
+          </Tag>
+        );
     }
   };
 
